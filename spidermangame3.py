20ff6a2ad206bdfd3664989a36167ae42db3c825
Aravi00 <147725768+Aravi00@users.noreply.github.com>
2023-11-06 09:39:07 -0500

    Update spidermangame3.py

--- spidermangame3.py
+++ spidermangame3.py
@@ -8,21 +8,39 @@ window = py.display.set_mode((windowwidth,windowheight))
 clock = py.time.Clock()
 #loop
 rext = 50
-reyt =50
+reyt = 50
 yspeed = 2
 pressed = False
+def getdata (boxx,boxy,mousex,mousey):
+    global rext, reyt
+    length = np.sqrt((mousex-(boxx+30))**2 + (boxy-mousey)**2)
+    if mousex>(boxx+30):
+        x = mousex-(boxx+30)
+    elif mousex<(boxx+30):
+        x = (boxx+30)-mousex
+    if mousey>boxy:
+        y = boxy-mousey
+    elif mousey<boxy:
+        y = mousey-boxy
+    if (mousex<(boxx+30) and mousey<boxy) or (mousex>(boxx+30) and mousey>boxy):
+        angle = np.degrees(np.arctan(x/y))
+        print("topl")
+    if (mousex<(boxx+30) and mousey>boxy) or (mousex>(boxx+30) and mousey<boxy):
+        angle = np.degrees(np.arctan(y/x))
+        print("topr")
+    return angle, length
 while True:
     ev = py.event.poll()
     if ev.type == py.QUIT:
         break
     key = py.key.get_pressed()
     if ev.type == py.MOUSEBUTTONDOWN:
-        pressed = True
+        pressed = not pressed
         x,y = py.mouse.get_pos()
         print(x,y)
-        
-    window.fill("white")    
-    yspeed+=1
+        angle,length = getdata(rext,reyt,x,y)
+    window.fill("white")
+    yspeed+=0.05
     reyt+=yspeed
     if reyt >= 400:
         yspeed = 0
@@ -32,21 +50,18 @@ while True:
         rext = 0
     elif rext < 0:
         rext = windowwidth
-    
-    rect = [rext,reyt,30,40] #x,y,w,h
-    #draw here
+        #draw here
     if pressed:
-        angle = int(np.degrees(np.arctan((reyt-y)/(x-(rext+30)))))
-        length = np.sqrt((x-(rext+30))**2 + (reyt-y)**2)
         print(angle,length)
-        
-        
-        
-        
-        
+        rext = x - length*(np.cos(angle))
+        reyt = y + length*np.sin(angle)
+        angle+=0.01
         py.draw.line(window,(0,0,0),(rext+30,reyt),(x,y))
+        
+    rect = [rext,reyt,30,40] #x,y,w,h
     py.draw.rect(window,(0,0,0),rect)
-    py.display.flip() 
-    clock.tick(60)                                   
+    py.display.flip()
+    clock.tick(60)     
+    
 py.quit()
 
